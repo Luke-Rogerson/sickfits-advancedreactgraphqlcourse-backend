21ac7c0bdfe13045809aa0aabc0e5d26ab905c8c
Luke Rogerson <lukerogerson@aol.com>
2018-12-29 16:52:16 +0000

    First commit

---
+++ src/db.js
@@ -0,0 +1,12 @@
+// Connects to remote PRISMA DB and allows us to query it with Javascript
+
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+})
+
+module.exports = db;
---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,40 @@
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // make a copy of the updates
+    const updates = { ...args };
+    // remove ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // 2. check if they own it or have permissions
+    // TODO
+    // 3. Delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
+};
+
+module.exports = Mutations;
