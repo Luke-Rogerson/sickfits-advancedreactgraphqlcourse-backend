2d454ff31b9781f995dcfc606948e60224586e74
Luke Rogerson <lukerogerson@aol.com>
2018-12-31 16:52:10 +0000

    Get curently user info

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const cookieParser = require('cookie-parser');
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -8,6 +8,17 @@ const server = createServer();
 
 // Use express middleware to handle cookies
 server.express.use(cookieParser());
+
+// decode the JWT to get the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userID onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
