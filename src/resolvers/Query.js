9a09f7a19b7ab8350a2b963c1e1bcf8af4d3f072
Luke Rogerson <lukerogerson@aol.com>
2019-01-06 20:02:08 +0000

    Create query to get all users from db

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -15,6 +16,16 @@ const Query = {
       },
       info
     );
+  },
+  async users(parent, args, ctx, info) {
+    // 1. Check if they are logged in
+    if (!ctx.request.userId) throw new Error('You must be logged in!');
+    // 2. Check if the user has the permission to query all the users
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    // 3. If they do, query all the users
+    return await ctx.db.query.users({}, info);
+    // Empty where object, info will include the GraphQL query that contains
+    // the fields that we're requesting from frontend
   }
 };
 
