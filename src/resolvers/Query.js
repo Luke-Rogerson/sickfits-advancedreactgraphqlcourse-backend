26b0554438bf1e9f547f77964a0826dff9a8ffa0
Luke Rogerson <lukerogerson@aol.com>
2019-01-10 15:33:35 +0000

    Add query to fetch a specific order details

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -26,6 +26,29 @@ const Query = {
     return await ctx.db.query.users({}, info);
     // Empty where object, info will include the GraphQL query that contains
     // the fields that we're requesting from frontend
+  },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You are not logged in!');
+    }
+    // 2. Query the current order
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id }
+      },
+      info
+    );
+    // 3. Check if the have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      'ADMIN'
+    );
+    if (!ownsOrder && !hasPermissionToSeeOrder) {
+      throw new Error('Not allowed');
+    }
+    // 4. Return the order
+    return order;
   }
 };
 
